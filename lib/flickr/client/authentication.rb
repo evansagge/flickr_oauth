5eb875f3d075414da7f1ee3cdcbdc3c43f07d43f
Evan Sagge <evansagge@gmail.com>
2011-07-07 10:28:03 +0800

    Re-do authentication_options

--- lib/flickr/client/authentication.rb
+++ lib/flickr/client/authentication.rb
@@ -1,15 +1,14 @@
 module Flickr
   class Client
     module Authentication
-      AUTHENTICATION_KEYS = [:consumer_key, :consumer_secret, :token, :token_secret]
       
       def authentication_options(options = {})
         {
-          :consumer_key => consumer_key,
-          :consumer_secret => consumer_secret,
-          :token => token,
-          :token_secret => token_secret
-        }.merge(options.select{|k,v| AUTHENTICATION_KEYS.include?(k.to_sym)})
+          :consumer_key => options[:consumer_key] || consumer_key,
+          :consumer_secret => options[:consumer_secret] || consumer_secret,
+          :token => options[:token] || token,
+          :token_secret => options[:token_secret] || token_secret
+        }
       end
       
     end
